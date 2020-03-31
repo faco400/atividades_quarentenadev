a3235d3f7314b60c1602a81bb7cfb75f44d3f20a
faco400 <49957412+faco400@users.noreply.github.com>
2020-03-31 13:23:01 -0300

    Aula2_IdadeMedia

---
+++ idade_Media.js
@@ -0,0 +1,13 @@
+d_Gui = 30
+30
+Id_Carol = 20
+20
+Id_Marcia = 15
+15
+Id_Patricia = 10
+10
+ID_Soma = Id_Gui + Id_Carol + Id_Marcia + Id_Patricia
+75
+id_Media = ID_Soma/4
+18.75
+document.write("A idade media da familia de Guilierme e: ",id_Media)
